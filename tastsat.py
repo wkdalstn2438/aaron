b8a816bbcb9586b7e60a4be50c5e90226c194769
aaron926 <69377185+aaron926@users.noreply.github.com>
2020-09-19 13:41:41 +0900

    Add files via upload

--- tastsat.py
+++ tastsat.py
@@ -63,7 +63,7 @@ for i in range(100):
 if i > 50:
     print("not find value")
 else:
-    print("find", i)
+    print("find", i) 
 """
 """
 b = input()
@@ -76,10 +76,83 @@ for i in range(len(b)):
 flag = 0
 """"""
 a = ([1, 2, 3,], [4, 5, 6,])
-print(a[1][2])"""
-def add_test(x, y):
+print(a[1][2])
 
-    return x + y
+import random
 
-c = add_test(1, 3)
-print(c)
+class makecharacter:
+    def __init__(self, id):
+        self.name = id
+        self.hp = random.randrange(475, 900)
+        self.mp = random.randrange(430, 600)
+        self.p = random.randrange(57, 80)
+        self.s = random.randrange(171, 240)
+        print("{}가 생성되었습니다.\nhp:{}\nmp:{}\n공격력:{}".format(self.name, self.hp, self.mp, self.p))
+    def attack(self):
+        return self.p
+
+    def critical(self):
+        ran = 2
+        self.p * ran
+        return self.p
+
+    def attacked(self, atk):
+        self.hp = self.hp - atk
+
+    def skill(self):
+       self.mp = self.mp - 150
+       return self.s
+
+
+
+id = input("Enter characer name : ")
+
+ogar = makecharacter(id)
+
+id = input("Enter characer name : ")
+
+mc = makecharacter(id)
+
+
+
+while True:
+    s = random.randrange(1, 5)
+    if s == 3:
+        mc.attacked(ogar.skill())
+    else:
+        c = random.randrange(1, 10)
+        if c == 3:
+            mc.attacked(ogar.critical())
+        else: mc.attacked(ogar.attack())
+        if mc.hp <= 0:
+            print("{} 사망".format(mc.name))
+            break
+
+
+
+    print("after mc hp: ", mc.hp)
+    print("after ogar mp: ", ogar.mp)
+    print("before ogar hp: ", ogar.hp)
+
+    print("before mc mp: ", mc.mp)
+    s = random.randrange(1, 5)
+    if s == 3:
+        ogar.attacked(mc.skill())
+    else:
+        c = random.randrange(1, 10)
+        if c == 3:
+            ogar.attacked(mc.critical())
+        else:ogar.attacked(mc.attack())
+        if ogar.hp <= 0:
+            print("{} 사망".format(ogar.name))
+            break
+
+    print("after ogar hp: ", ogar.hp)
+    print("after mc mp: ", mc.mp)"""
+
+f = open("/Users/scratch1/Desktop/test.txt", "r")
+
+teep = f.readlines()
+
+print(teep)
+
