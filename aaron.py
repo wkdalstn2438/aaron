a950e98d7e93b4ddfb2fd97ef5f2d09b2384b314
aaron926 <69377185+aaron926@users.noreply.github.com>
2021-12-28 21:38:33 +0900

    Add files via upload

--- aaron.py
+++ aaron.py
@@ -1,49 +1,23 @@
-# <변수>
-# 자료형 = 어떤 종류의 값을 담을 수 있는가
-# 123 정수형 값 = int형 a = 1
-#
-# "abc" 'ㄱㄴㄷ' = str형(string)
-#
-# 3.14 = float형
-#
-#
-# + - * / % //는 소수점 제거
+# a = list(map(int, input().split(" ")))
+import random
 
-# input() 어떤것이든 str형로 저장된다
+v = int(input())
+a = []
+b = 0
 
-# bool은 ture 와 false 밖에 없다
-# ture와 false도 값이다
-
-
-
-# <조건부>
-# if 조건:
-#     실행문
-
-# >, <, ==,!=, <=, >=  ==는 같다면 이다
-# or 는 둘중 하나만 만 되도 작동한다, and는 모두참이어야한다 ()는 우선 순위를 나누어 준다.
-
-# elif는 if 가 성립되면은 무시된다  else는 모두 안될 경우에 성립된다
-
-
-# <반복문>
-
-# for i in range(반복횟수):
-
-# a = int(input())
-#
-# if a % 2 == 1:
-#     print("odd")
-#
-# if a == 0:
-#     print("0")
-#
-# elif a % 2 == 0:
-#     print("even")
-
-# for i in range(1,11):
-#     print("*"*i)
-
-# for i in range(1,11):
-#     print("*"*(11-i))
-
+for i in range(v):
+    a.append(random.randrange(50))
+print(a)
+while True:
+    if b == len(a)-1:
+        break
+    b = 0
+    for i in range(len(a)-1):
+        if a[i] <= a[i + 1]:
+            b = b + 1
+    for i in range(len(a)-1):
+        if a[i] > a[i + 1]:
+            c = a[i + 1]
+            a[i + 1] = a[i]
+            a[i] = c
+print(a)
